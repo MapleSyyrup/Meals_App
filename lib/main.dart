d2a6f39a4030e72407ec4b36897f28ba2d33fc4c
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2020-12-08 16:48:21 +0800

    162-163. Using Named Routes and Passing Data - Use named routes to easily manage routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 import './categories_screen.dart';
 
@@ -26,7 +27,12 @@ class MyApp extends StatelessWidget {
         )
       ),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
